5d55afa90a916faca962698f4e301de0902c6985
chaoyuu <chaoyu995@gmail.com>
2021-07-08 15:37:02 +1000

    add brown

--- components/Colours.js
+++ components/Colours.js
@@ -1,8 +1,8 @@
 export const Colours = {
     light: 'hsla(0, 0%, 94%, 1)',
-    powderblue: 'hsla(228, 63%, 51%, 1)',
-    primary: 'hsla(173, 63%, 51%, 1)',
+    secondary: 'hsla(115, 53%, 51%, 1)',
+    primary: 'hsla(200, 63%, 51%, 1)',
     dark: 'hsla(0, 0%, 3%, 1)',
     tertiary: 'hsla(345, 63%, 51%, 1)',
+    brown:'hsla(10, 1%, 3%, 5)',
   }
-  
